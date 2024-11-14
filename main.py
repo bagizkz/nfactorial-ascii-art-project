e5f074fd55a9d021b551bee93f673b3efff7b81b
bagizkz <or.baglane@gmail.com>
2024-11-15 03:05:11 +0500

    add - Запись в файл

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import sys
+import argparse
 
 ###(1) todo: добавить возможность менять стиль shadow.txt, thinkertoy > [STRING] [BANNER]
-#(0) todo: добавить возможность сохранить в файл который юзер указал > [OPTION] [STRING] [BANNER]
+###(1) todo: добавить возможность сохранить в файл который юзер указал > [OPTION] [STRING] [BANNER]
 #(0) todo: возможность выводить символы в цветном виде > [OPTION] [STRING] [BANNER]
-#(0) todo: оптимизровать код, исправить
+#(0) todo: возможность justify, align ровнять центр или вправо  > [OPTION] [STRING] [BANNER]
+
+#(0) todo: оптимизровать код, исправить привести в порядок комменты
 
 
 
@@ -13,8 +16,8 @@ def load_font(file_path):
     with open(file_path, 'r') as f:
         lines = f.read().splitlines()
         
-        char_h = 8  #высота
-        index = 0 #начинаем c 0
+        char_h = 8  # высота символа
+        index = 0  # начинаем с 0
         
         for ascii_code in range(32, 127):  # ASCII-символы
             char = chr(ascii_code)
@@ -22,17 +25,15 @@ def load_font(file_path):
             font[char] = char_representation
             index += char_h + 1  # переходим к следующему символу
     
-    if ' ' not in font:     # Если пробела нет
+    if ' ' not in font:  # если пробела нет
         font[' '] = [' ' * len(font['!'][0]) for _ in range(char_h)]
     
-    return font #Возвращаем
+    return font  # возвращаем
 
 
-
-
-# Рендерим
+# рендерим
 def render_text(input_text, font):
-    lines = ['' for _ in range(8)]  
+    lines = ['' for _ in range(8)]
     
     for char in input_text:
         if char == '\n':
@@ -44,24 +45,22 @@ def render_text(input_text, font):
             for i in range(8):
                 lines[i] += char_representation[i]
         else:
-             # Если символа нет добавляем пробелы
+            # если символа нет добавляем пробелы
             for i in range(8):
                 lines[i] += ' ' * len(font[' '][i])
     
-    return '\n'.join(lines)   # возвращаем
+    return '\n'.join(lines)  # возвращаем
 
 
-
-
-### 
 def main():
-    if len(sys.argv) != 3:
-        print("Используйте команду: python3 main.py <STR> <BANNER>")
-        print("BANNER: standard, shadow, thinkertoy")
-        return
-    
-    input_text = sys.argv[1]
-    banner = sys.argv[2].lower()
+    # Настройка аргументов командной строки
+    parser = argparse.ArgumentParser(description="Рендеринг ASCII-арт текста.")
+    parser.add_argument("input_text", type=str, help="Текст для рендеринга.")
+    parser.add_argument("banner", type=str, help="BANNER: standard, shadow, thinkertoy")
+    parser.add_argument("-o", "--output", type=str, help="Файл для сохранения", default=None)
+    args = parser.parse_args()
+
+    banner = args.banner.lower()
     
     # Определяем путь к файлу
     font_files = {
@@ -76,8 +75,15 @@ def main():
         return
     
     font = load_font(font_files[banner])  # загрузка выбранного шрифта
-    ascii_art = render_text(input_text, font)
-    print(ascii_art)
+    ascii_art = render_text(args.input_text, font)
+    
+    # Если указан output_file, записываем результат в файл или выводим в терминал
+    if args.output:
+        with open(args.output, 'w') as output_file:
+            output_file.write(ascii_art)
+    else:
+        print(ascii_art)
+
 
 if __name__ == "__main__":
     main()
