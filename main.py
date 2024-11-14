7918b8144a11d69e584ac49375e31d08dd34d74f
bagizkz <or.baglane@gmail.com>
2024-11-15 01:05:21 +0500

    возможность менять стиль shadow.txt, thinkertoy > [STRING] [BANNER]

--- main.py
+++ main.py
@@ -1,60 +1,83 @@
 import sys
 
-
-#todo: добавить возможность менять стиль shadow.txt, thinkertoy > [STRING] [BANNER]
-#todo: добавить возможность сохранить в файл который юзер указал > [OPTION] [STRING] [BANNER]
-#todo: возможность выводить символы в цветном виде > [OPTION] [STRING] [BANNER]
-#todo: оптимизровать код, исправить
+###(1) todo: добавить возможность менять стиль shadow.txt, thinkertoy > [STRING] [BANNER]
+#(0) todo: добавить возможность сохранить в файл который юзер указал > [OPTION] [STRING] [BANNER]
+#(0) todo: возможность выводить символы в цветном виде > [OPTION] [STRING] [BANNER]
+#(0) todo: оптимизровать код, исправить
 
 
 
 # подгрузим шаблон
-def load_txt(file_path): 
-    font = {} # словарь
+def load_font(file_path):
+    font = {}
     with open(file_path, 'r') as f:
-        lines = f.read().splitlines() # Читаем файл и делим его строки
+        lines = f.read().splitlines()
         
-        char_h = 8 #высота
+        char_h = 8  #высота
         index = 0 #начинаем c 0
         
-        for ascii_code in range(32, 127):
+        for ascii_code in range(32, 127):  # ASCII-символы
             char = chr(ascii_code)
-            char_rep = lines[index : index + char_h]
-            font[char] = char_rep
-            index += char_h + 1 
+            char_representation = lines[index : index + char_h]
+            font[char] = char_representation
+            index += char_h + 1  # переходим к следующему символу
     
-    if ' ' not in font:     # Если пробела нет добавляем
+    if ' ' not in font:     # Если пробела нет
         font[' '] = [' ' * len(font['!'][0]) for _ in range(char_h)]
     
-    return font # Возвращаем
+    return font #Возвращаем
+
+
 
 
 # Рендерим
-def render_text(text, font):
-    result = ['' for _ in range(8)]  # Записывать строки для картинки
-
-    for char in text:
-        if char == '\n':  # Если перенос строки
-            print('\n'.join(result))  # Печатаем и очищаем
-            result = ['' for _ in range(8)]  # Очищаем, чтобы начать с новой строки
-        elif char in font:  # Если символ есть в тхт
+def render_text(input_text, font):
+    lines = ['' for _ in range(8)]  
+    
+    for char in input_text:
+        if char == '\n':
+            lines = ['' for _ in range(8)]
+            print('\n'.join(lines))
+            continue
+        if char in font:
+            char_representation = font[char]
             for i in range(8):
-                result[i] += font[char][i]  # Добавляем строки св результат
-        else:  # Если символа нет просто добавляем пробелы
+                lines[i] += char_representation[i]
+        else:
+             # Если символа нет добавляем пробелы
             for i in range(8):
-                result[i] += ' ' * len(font[' '][i]) 
+                lines[i] += ' ' * len(font[' '][i])
+    
+    return '\n'.join(lines)   # возвращаем
 
-    return '\n'.join(result)  # возвращаем
+
+
 
 ### 
 def main():
-    input_text = sys.argv[1] # Получаем текст
-    font = load_txt('standard.txt')  # Загружаем из txt
-    ascii_art = render_text(input_text, font) # Рендим текст
-    print(ascii_art) #выдаем
-
-
-
+    if len(sys.argv) != 3:
+        print("Используйте команду: python3 main.py <STR> <BANNER>")
+        print("BANNER: standard, shadow, thinkertoy")
+        return
+    
+    input_text = sys.argv[1]
+    banner = sys.argv[2].lower()
+    
+    # Определяем путь к файлу
+    font_files = {
+        'standard': 'standard.txt',
+        'shadow': 'shadow.txt',
+        'thinkertoy': 'thinkertoy.txt'
+    }
+    
+    # Проверяем, что указанный BANNER доступен
+    if banner not in font_files:
+        print(f"Такого BANNER - '{banner}' НЕТ! Доступно: standard, shadow, thinkertoy")
+        return
+    
+    font = load_font(font_files[banner])  # загрузка выбранного шрифта
+    ascii_art = render_text(input_text, font)
+    print(ascii_art)
 
 if __name__ == "__main__":
     main()
